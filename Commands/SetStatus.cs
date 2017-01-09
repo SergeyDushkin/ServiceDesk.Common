8e320b39a10482ecd967461648736081f5463c37
SergeyDushkin <leirbythe@gmail.com>
2017-01-09 13:59:50 +0300

    set ICommand for SetStatus

--- Commands/SetStatus.cs
+++ Commands/SetStatus.cs
@@ -3,7 +3,7 @@ using Coolector.Common.Commands;
 
 namespace servicedesk.Common.Commands
 {
-    public class SetStatus
+    public class SetStatus : ICommand
     {
         public Request Request { get; set; }
         public Guid SourceId { get; set; }
