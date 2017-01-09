12c7755bb58908b36327d464e07a14f66662764f
SergeyDushkin <leirbythe@gmail.com>
2017-01-09 12:47:55 +0300

    init

---
+++ Commands/SetStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace servicedesk.Common.Commands
+{
+public class Request
+{
+    public DateTime CreatedAt { get; set; }
+    public string Culture { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Origin { get; set; }
+    public string Resource { get; set; }
+}
+
+public class SetStatus
+{
+    public Request Request { get; set; }
+    public Guid SourceId { get; set; }
+    public Guid ReferenceId { get; set; }
+    public Guid StatusId { get; set; }
+    public string UserId { get; set; }
+    public string Message { get; set; }
+}
+}
