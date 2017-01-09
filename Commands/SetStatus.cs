2e6c32481ed4eba20613ef8b31f5b0d8711ea58c
SergeyDushkin <leirbythe@gmail.com>
2017-01-09 13:53:22 +0300

    add Collector reference

--- Commands/SetStatus.cs
+++ Commands/SetStatus.cs
@@ -1,24 +1,15 @@
 ﻿using System;
+using Coolector.Common.Commands;
 
 namespace servicedesk.Common.Commands
 {
-public class Request
-{
-    public DateTime CreatedAt { get; set; }
-    public string Culture { get; set; }
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Origin { get; set; }
-    public string Resource { get; set; }
-}
-
-public class SetStatus
-{
-    public Request Request { get; set; }
-    public Guid SourceId { get; set; }
-    public Guid ReferenceId { get; set; }
-    public Guid StatusId { get; set; }
-    public string UserId { get; set; }
-    public string Message { get; set; }
-}
+    public class SetStatus
+    {
+        public Request Request { get; set; }
+        public Guid SourceId { get; set; }
+        public Guid ReferenceId { get; set; }
+        public Guid StatusId { get; set; }
+        public string UserId { get; set; }
+        public string Message { get; set; }
+    }
 }
