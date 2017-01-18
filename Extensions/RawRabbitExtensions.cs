2b1d6598c57902d684b57e66f4f2df570745dd6c
SergeyDushkin <leirbythe@gmail.com>
2017-01-18 19:23:38 +0300

    set auto Queue-Name

--- Extensions/RawRabbitExtensions.cs
+++ Extensions/RawRabbitExtensions.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using RawRabbit;
 using RawRabbit.Common;
 using servicedesk.Common.Commands;
@@ -8,6 +7,15 @@ namespace servicedesk.Common.Extensions
 {
     public static class RawRabbitExtensions
     {
+        public static ISubscription WithCommandHandlerAsync<TCommand>(this IBusClient bus,
+            ICommandHandler<TCommand> handler, string name = null) where TCommand : ICommand
+            => bus.SubscribeAsync<TCommand>(async (msg, context) => await handler.HandleAsync(msg)); 
+
+        public static ISubscription WithEventHandlerAsync<TEvent>(this IBusClient bus,
+            IEventHandler<TEvent> handler, string name = null) where TEvent : IEvent
+            => bus.SubscribeAsync<TEvent>(async (msg, context) => await handler.HandleAsync(msg));
+
+        /*
         public static ISubscription WithCommandHandlerAsync<TCommand>(this IBusClient bus,
             ICommandHandler<TCommand> handler, string name = null) where TCommand : ICommand
         => bus.SubscribeAsync<TCommand>(async (msg, context) => await handler.HandleAsync(msg),
@@ -21,6 +29,6 @@ namespace servicedesk.Common.Extensions
         private static string GetExchangeName<T>(string name = null)
             => string.IsNullOrWhiteSpace(name)
                 ? $"{Assembly.GetEntryAssembly().GetName()}/{typeof(T).Name}"
-                : $"{name}/{typeof(T).Name}";
+                : $"{name}/{typeof(T).Name}";*/
     }
 }
